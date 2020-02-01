38edb72e464843733c454b22a8bbbe4d1b410e7b
Chris Dusyk <chrisd@iqmetrix.com>
2020-01-31 23:42:02 -0600

    Add more base stubs

---
+++ The Scavenger/Assets/Scripts/Robots/Components/RobotMovementData.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct RobotMovementData : IComponentData
+{
+	public Vector2 direction;
+	public float speed;
+}
---
+++ The Scavenger/Assets/Scripts/Robots/Systems/RobotMovementSystem.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+public class RobotMovementSystem : JobComponentSystem
+{
+	protected override JobHandle OnUpdate(JobHandle inputDeps)
+	{
+		JobHandle robotMovementHandle = Entities.ForEach((ref Transform transform, in RobotMovementData robotMovementData) =>
+		{
+
+		}).Schedule(inputDeps);
+
+		return robotMovementHandle;
+	}
+}
